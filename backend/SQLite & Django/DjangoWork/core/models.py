02cae5ed0093685da27dcb8ddb8104873cfafa10
TonioQ <123836145+TonioQ@users.noreply.github.com>
2024-11-15 18:23:58 -0500

    Update models.py
    Updated model to include state/counties and bookmark

--- backend/SQLite & Django/DjangoWork/core/models.py
+++ backend/SQLite & Django/DjangoWork/core/models.py
@@ -5,12 +5,15 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=15)
 
     def __str__(self):
         return self.username
     
 # Create Traffic Law Model
 class TrafficLaw(models.Model):
+    state = models.CharField(max_length=50)
+    county = models.CharField(max_length=50)
     law_code = models.CharField(max_length=10, unique=True)
     description = models.TextField()
     date = models.DateField()
@@ -23,6 +26,14 @@ class Violation(models.Model):
     violation_code = models.CharField(max_length=10, unique=True)
     violation_law = models.ForeignKey(TrafficLaw, on_delete=models.CASCADE, related_name='violations')
     penalty = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.violation
+    
+# Create bookmark model
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookmarks')
+    violation = models.ForeignKey(Violation, on_delete=models.CASCADE, related_name='bookmarks')
 
     def __str__(self):
         return self.violation
